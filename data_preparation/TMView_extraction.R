9293ea84efd9f028233dfb88c31d99559ac08305
KAZIMIERCZAK Michal Andrzej <Michal.KAZIMIERCZAK@euipo.europa.eu>
2020-04-08 18:28:07 +0200

    new change

--- data_preparation/TMView_extraction.R
+++ data_preparation/TMView_extraction.R
@@ -4,4 +4,19 @@
 
 ####Read the csv files of the TMView repository
 
-setwd("/TMView_data/")
+getwd()
+setwd("data_preparation/TMView_data")
+lista<-list.files("tmdsv")
+library(stringr)
+
+i<-1
+
+for (i in 1:length(lista))
+{
+  c<-read.csv(unzip(paste("tmdsv/",lista[i],sep="")))
+  write.csv(c,paste("TM/",str_replace(lista[i],"-TMV\\.zip",".csv"),sep=""))
+}
+
+c<-read.csv("tmdsv/AT-TMV/AT-TMV.csv")
+
+remove(c)
