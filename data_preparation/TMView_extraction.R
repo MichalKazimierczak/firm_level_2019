27548332937ee53e251db3bab51e68e7c4a6ba7a
KAZIMIERCZAK Michal Andrzej <Michal.KAZIMIERCZAK@euipo.europa.eu>
2020-04-09 11:57:59 +0200

    09/04/2020

--- data_preparation/TMView_extraction.R
+++ data_preparation/TMView_extraction.R
@@ -9,14 +9,37 @@ setwd("data_preparation/TMView_data")
 lista<-list.files("tmdsv")
 library(stringr)
 
-i<-1
+i<-8
 
-for (i in 1:length(lista))
+for (i in c(6,7,8))
 {
   c<-read.csv(unzip(paste("tmdsv/",lista[i],sep="")))
-  write.csv(c,paste("TM/",str_replace(lista[i],"-TMV\\.zip",".csv"),sep=""))
+  write.csv(c,paste("TM/",str_replace(lista[i],"-TMV-(.)\\.zip","_\\1.csv"),sep=""))
 }
 
 c<-read.csv("tmdsv/AT-TMV/AT-TMV.csv")
 
 remove(c)
+bx<-read.csv("DE-TMV-1.csv")
+
+
+###check the columns available in each country
+setwd("TM")
+getwd()
+lista<-list.files()
+i<-1
+cols<-data.frame()
+for (i in 1:length(lista))
+{
+  c<-read.csv(lista[i],nrows=10)
+  col<-colnames(c)[-1]
+  c<-cbind(country=lista[i],col)
+  c<-as.data.frame(c)
+  cols<-rbind(cols,c)
+}
+
+
+unique(cols$col)
+
+
+cz<-read.csv("DK.csv",nrows=10000)
